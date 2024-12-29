ad3eb6c52639de7d7ecff501d8f2287555cc2ff9
king7hills <haasjh1@gmail.com>
2024-12-28 23:51:13 -0500

    Added round function and game function. Update to README

--- README.md
+++ README.md
@@ -7,3 +7,15 @@ Start date: 12.28.24
 The initial version will be a game played within the console. Later on, a GUI will be added.
 
 Computer choice is determined by a random number. Choice ranges are divided into thirds.
+Human choice is an input. Currently works if the user inputs any case version of rock paper or scissors. Malfunctions for other words.
+
+Rounds compare each possible outcome. Not sure if there is a more efficient way to compare the variables.
+The game uses a while loop, updating the roundsWon variable each time a round is won.
+In real life rock, paper, scissors, ties do not count toward 'best of' matches. The same is true here.
+
+I had some trouble initially with the while loop. I realized after adding a console.log to my round keeping variable
+that the rounds were not updating. My variables were not within the while loop. This correction and adding an update after the round
+function ran solved the issue.
+
+The game works now. It is by no means spectacular, but it completes the assignment. I am following the recommendation to not embellish this project
+and save my time and energy for portfolio pieces.
--- rps.js
+++ rps.js
@@ -16,6 +16,7 @@ function getComputerChoice() {
     else return scissors;
 }
 
+//Function for the player's choice
 function getHumanChoice(input) {
     let choice = prompt("Please type rock, paper, or scissors.")
     if (choice.toLowerCase() == rock) {
@@ -27,4 +28,49 @@ function getHumanChoice(input) {
     } else return "Please refresh and try again"
 }
 
-console.log(getHumanChoice())
+//Score variables
+let humanScore = 0;
+let computerScore = 0;
+
+//Round function. Compares choices, awards winner a point, and alerts user to who won.
+function playRound(humanChoice, computerChoice) {
+    if (humanChoice == rock && computerChoice == scissors) {
+        humanScore = humanScore + 1;
+        console.log('Great! You win this round.')
+    } else if (humanChoice == paper && computerChoice == rock) {
+        humanScore = humanScore + 1;
+        console.log('Great! You win this round.')
+    } else if (humanChoice == scissors && computerChoice == paper) {
+        humanScore = humanScore + 1;
+        console.log('Great! You win this round.')
+    } else if (humanChoice == rock && computerChoice == paper) {
+        computerScore = computerScore + 1;
+        console.log('Yikes! You lose this round.')
+    } else if (humanChoice == paper && computerChoice == scissors) {
+        computerScore = computerScore + 1;
+        console.log('Yikes! You lose this round.')
+    } else if (humanChoice == scissors && computerChoice == rock) {
+        computerScore = computerScore + 1;
+        console.log('Yikes! You lose this round.')
+    } else {console.log("Tie! Go again.")}
+}
+
+//Game function
+function playGame () {
+    let roundsWon = humanScore + computerScore;
+    while (roundsWon < 5) {
+        //Store choices as variables
+        let humanSelection = getHumanChoice();
+        let computerSelection = getComputerChoice();
+        //Play a round
+        playRound(humanSelection, computerSelection);
+        //updates roundsWon within the while loop. Ties are not counted (just like real life)
+        roundsWon = humanScore + computerScore;
+        //used for debug console.log(roundsWon)
+    }
+    if (humanScore > computerScore) {
+        console.log("You win! :)");
+    } else console.log("You lose. :(");
+}
+
+playGame()
